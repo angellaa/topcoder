5e2ab28b0c8df2c9f604cedefc93999bc198044e
Andrea Angella <angella.andrea@gmail.com>
2014-08-10 21:02:29 +0100

    Added implementation of Time

---
+++ SRM-144/Time.cs
@@ -0,0 +1,84 @@
+// BEGIN CUT HERE
+
+// END CUT HERE
+using System;
+
+public class Time 
+{
+    public string whatTime(int seconds) 
+    {
+        return new TimeSpan(0, 0, seconds).ToString(@"h\:m\:s");
+    }
+
+// BEGIN CUT HERE
+    public static void Main(String[] args) {
+        try  {
+            eq(0,(new Time()).whatTime(0),"0:0:0");
+            eq(1,(new Time()).whatTime(3661),"1:1:1");
+            eq(2,(new Time()).whatTime(5436),"1:30:36");
+            eq(3,(new Time()).whatTime(86399),"23:59:59");
+        } 
+        catch( Exception exx)  {
+            System.Console.WriteLine(exx);
+            System.Console.WriteLine( exx.StackTrace);
+        }
+    }
+    private static void eq( int n, object have, object need) {
+        if( eq( have, need ) ) {
+            Console.WriteLine( "Case "+n+" passed." );
+        } else {
+            Console.Write( "Case "+n+" failed: expected " );
+            print( need );
+            Console.Write( ", received " );
+            print( have );
+            Console.WriteLine();
+        }
+    }
+    private static void eq( int n, Array have, Array need) {
+        if( have == null || have.Length != need.Length ) {
+            Console.WriteLine("Case "+n+" failed: returned "+have.Length+" elements; expected "+need.Length+" elements.");
+            print( have );
+            print( need );
+            return;
+        }
+        for( int i= 0; i < have.Length; i++ ) {
+            if( ! eq( have.GetValue(i), need.GetValue(i) ) ) {
+                Console.WriteLine( "Case "+n+" failed. Expected and returned array differ in position "+i );
+                print( have );
+                print( need );
+                return;
+            }
+        }
+        Console.WriteLine("Case "+n+" passed.");
+    }
+    private static bool eq( object a, object b ) {
+        if ( a is double && b is double ) {
+            return Math.Abs((double)a-(double)b) < 1E-9;
+        } else {
+            return a!=null && b!=null && a.Equals(b);
+        }
+    }
+    private static void print( object a ) {
+        if ( a is string ) {
+            Console.Write("\"{0}\"", a);
+        } else if ( a is long ) {
+            Console.Write("{0}L", a);
+        } else {
+            Console.Write(a);
+        }
+    }
+    private static void print( Array a ) {
+        if ( a == null) {
+            Console.WriteLine("<NULL>");
+        }
+        Console.Write('{');
+        for ( int i= 0; i < a.Length; i++ ) {
+            print( a.GetValue(i) );
+            if( i != a.Length-1 ) {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine( '}' );
+    }
+// END CUT HERE
+}
